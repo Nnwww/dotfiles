3b9f1a85cfb571e9b5e30b30af37eb4de4fc145d
Nnwww <johndororo@gmail.com>
2016-04-13 18:33:13 +0900

    fix a script

--- dotlink.sh
+++ dotlink.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 [[ ! "$OSTYPE" == darwin* ]] && echo "This script only treat darwin now." && exit 2
 
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
@@ -9,11 +11,15 @@ mkdir -p ~/.config/nvim 2>/dev/null
 ln -sf ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/vim/.vim ~/.config/nvim
 
-if type nvim >/dev/null 2>&1; then
-    if type python3 >/dev/null 2>&1; then
+type nvim >/dev/null 2>&1
+if [ $? = 1 ]; then
+
+    type python3 >/dev/null 2>&1
+    if [ $? = 1 ]; then
         brew install python3
         pip3 install --upgrade pip setuptools
     fi
+
     pip3 install neovim
     brew install neovim/neovim/neovim
     echo "Execute the :UpdateRemotePlugins or :NeoBundleRemotePlugins (for using NeoBundle) in Neovim and restart it."
@@ -35,7 +41,8 @@ if [ ! -e ~/.zplug ]; then
     git clone https://github.com/b4b4r07/zplug ~/.zplug
 fi
 
-if type tmux >/dev/null 2>&1; then
+type tmux >/dev/null 2>&1
+if [ $? = 1 ]; then
     brew install tmux
     brew install reattach-to-user-namespace
 fi
