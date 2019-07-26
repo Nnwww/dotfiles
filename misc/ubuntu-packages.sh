bbff22b2e863cf59b43101bc69360c5a4fad9c05
harukaw-oikawa <haruka-oikawa@cybozu.co.jp>
2019-07-26 14:34:05 +0900

    I want install GUIenv...but this is not acommplished.

--- misc/ubuntu-packages.sh
+++ misc/ubuntu-packages.sh
@@ -16,21 +16,21 @@ apt-get -y install enpass
 sudo apt install libpulse0
 
 # install ripgrep
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
+curl -fsSLO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
 sudo dpkg -i ripgrep_11.0.1_amd64.deb
 rm ripgrep_11.0.1_amd64.deb
 
 # install fzf, use zplug on zsh
 
 # install fd
-curl -LO https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb
+curl -fsSLO https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb
 sudo dpkg -i fd_7.3.0_amd64.deb
 rm fd_7.3.0_amd64.deb
 
 # install exa
 
 # install bat
-curl -LO https://github.com/sharkdp/bat/releases/download/v0.11.0/bat_0.11.0_amd64.deb
+curl -fsSLO https://github.com/sharkdp/bat/releases/download/v0.11.0/bat_0.11.0_amd64.deb
 sudo dpkg -i bat_0.11.0_amd64.deb
 rm bat_0.11.0_amd64.deb
 
@@ -43,3 +43,18 @@ sudo apt-get -y install zsh
 # install tmux
 sudo apt-get -y install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#install rdp
+sudo apt-get -y install xrdp tightvncserver
+#link xsession
+
+# install lightdm
+sudo apt-get install lightdm
+sudo dpkg-reconfigure lightdm
+echo '[SeatDefaults]' | sudo tee /etc/lightdm/lightdm.conf.d/50-unity-greeter.conf
+echo 'greeter-session=unity-greeter' | sudo tee -a /etc/lightdm/lightdm.conf.d/50-unity-greeter.conf
+# install xmonad, and then put xmonad.hs into ~/.xmonad, xmonad --recompile
+
+curl -fsSLO 'https://dl.bintray.com/tigervnc/stable/ubuntu-16.04LTS/amd64/tigervncserver_1.7.1-1ubuntu1_amd64.deb'
+sudo dpkg -i tigervncserver_1.7.1-1ubuntu1_amd64.deb
+rm tigervncserver_1.7.1-1ubuntu1_amd64.deb
